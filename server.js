3371013ee8d10cd8ee7ded594d322e84e3f2fbb2
Ricardo Havill <rhavill@progresstesting.com>
2013-06-22 13:36:31 -0400

    Modified node server to serve static files.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+path = require("path"),
+application_root = __dirname,
 cases = require('./routes/cases');
  
 var app = express();
@@ -6,6 +8,7 @@ var app = express();
 app.configure(function () {
 app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
 app.use(express.bodyParser());
+app.use(express.static(path.join(application_root, "public")));
 });
  
 app.get('/cases', cases.findAll);
